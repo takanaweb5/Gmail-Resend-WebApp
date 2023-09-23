d758379720b3943673a7e9b35b11e25c5117771a
takanaweb5 <50874513+takanaweb5@users.noreply.github.com>
2023-09-24 08:11:24 +0900

    スレッドの最初のメールを対象にするよう変更

--- src/code.js
+++ src/code.js
@@ -1,7 +1,16 @@
 "use strict";
 function doGet(e) {
     const counter = makeDraftMail();
-    return ContentService.createTextOutput(`${counter}件の下書きを作成しました`).setMimeType(ContentService.MimeType.TEXT);
+    const message = `${counter}件の下書きを作成しました`;
+    Logger.log(message);
+    const html = e.parameter["h"];
+    if (html) {
+        // パラメータにhが指定されていたら、HTMLに変換
+        return HtmlService.createHtmlOutput(`<font size=100>${message}</font>`);
+    }
+    else {
+        return ContentService.createTextOutput(message).setMimeType(ContentService.MimeType.TEXT);
+    }
 }
 /**
  * 再送信ラベルのついたメールから下書きメールを作成する
@@ -10,20 +19,20 @@ function makeDraftMail() {
     // ラベルの名前（ここでは「再送信」）を指定
     const labelName = "再送信";
     const emailLabel = GmailApp.getUserLabelByName(labelName);
-    let count = 0;
+    let counter = 0;
     // ラベルが存在する場合
     if (emailLabel) {
         const threads = emailLabel.getThreads();
-        for (let i = 0; i < threads.length; i++) {
-            let message = getMessageWithOnlyMeAsSender(threads[i]);
-            if (message) {
-                createDraftFromMessage(message);
-                message.getThread().removeLabel(emailLabel);
-                count++;
+        for (const thread of threads) {
+            // メールスレッド（会話のやりとりを１つの塊にまとめたもの）の最古のメール
+            let message = thread.getMessages()[0];
+            if (createDraftFromMessage(message)) {
+                thread.removeLabel(emailLabel);
+                counter++;
             }
         }
     }
-    return count;
+    return counter;
 }
 /**
  * 送信済みメールから下書きメールを作成
@@ -31,40 +40,8 @@ function makeDraftMail() {
  * @return - なし
  */
 function createDraftFromMessage(message) {
-    // GmailMessageから受信者、件名、本文を取得
-    var recipient = message.getTo(); // 受信者のアドレス
-    var subject = message.getSubject(); // 件名
-    var body = message.getPlainBody(); // 本文（プレーンテキスト）
-    // 下書きメールを作成
-    var draft = GmailApp.createDraft(recipient, subject, body);
-    if (draft) {
-        Logger.log("下書きメールが作成されました。");
-    }
-    else {
-        Logger.log("下書きメールの作成に失敗しました。");
-    }
-}
-/**
- * メールスレッド（会話のやりとりを１つの塊にまとめたもの）の中から自分の送った最初の送信メールを取得する
- * @param thread - メールスレッド
- * @return 自分の送った最初の送信メール
- */
-function getMessageWithOnlyMeAsSender(thread) {
-    const myEmailAddress = Session.getActiveUser().getEmail(); // 自分のメールアドレスを取得
-    const messages = thread.getMessages(); // スレッド内のメッセージを取得
-    // for (let i = 0; i < messages.length; i++) {
-    //   Logger.log(i);
-    //   Logger.log(messages[i].getFrom());
-    //   Logger.log(messages[i].getTo());
-    //   Logger.log(messages[i].getSubject());
-    //   Logger.log(messages[i].getPlainBody());
-    //   }
-    const firstMessage = messages[0]; // スレッド内の最古のメッセージを取得
-    const sender = firstMessage.getFrom(); // 発信者を取得
-    // 最初のメッセージの発信者が自分のメールアドレスのみを含み、かつ複数の発信者ではない場合
-    if (sender.includes(myEmailAddress) && !sender.includes(",")) {
-        return firstMessage; // メッセージを返す
-    }
-    // 条件を満たすメッセージが見つからない場合
-    return null;
+    const recipient = message.getTo(); // 受信者のアドレス
+    const subject = message.getSubject(); // 件名
+    const body = message.getPlainBody(); // 本文（プレーンテキスト）
+    return Boolean(GmailApp.createDraft(recipient, subject, body));
 }
--- ts/code.ts
+++ ts/code.ts
@@ -1,6 +1,15 @@
 function doGet(e: GoogleAppsScript.Events.DoGet): any {
   const counter = makeDraftMail();
-  return ContentService.createTextOutput(`${counter}件の下書きを作成しました`).setMimeType(ContentService.MimeType.TEXT);
+  const message = `${counter}件の下書きを作成しました`;
+  Logger.log(message);
+
+  const html = e.parameter["h"];
+  if (html) {
+    // パラメータにhが指定されていたら、HTMLに変換
+    return HtmlService.createHtmlOutput(`<font size=100>${message}</font>`)
+  } else {
+    return ContentService.createTextOutput(message).setMimeType(ContentService.MimeType.TEXT);
+  }
 }
 
 /**
@@ -10,20 +19,20 @@ function makeDraftMail(): number {
   // ラベルの名前（ここでは「再送信」）を指定
   const labelName = "再送信";
   const emailLabel: GoogleAppsScript.Gmail.GmailLabel = GmailApp.getUserLabelByName(labelName);
-  let count: number = 0;
+  let counter: number = 0;
   // ラベルが存在する場合
   if (emailLabel) {
     const threads: GoogleAppsScript.Gmail.GmailThread[] = emailLabel.getThreads();
-    for (let i = 0; i < threads.length; i++) {
-      let message: GoogleAppsScript.Gmail.GmailMessage | null = getMessageWithOnlyMeAsSender(threads[i]);
-      if (message) {
-        createDraftFromMessage(message);
-        message.getThread().removeLabel(emailLabel);
-        count++;
+    for (const thread of threads) {
+      // メールスレッド（会話のやりとりを１つの塊にまとめたもの）の最古のメール
+      let message: GoogleAppsScript.Gmail.GmailMessage = thread.getMessages()[0];
+      if (createDraftFromMessage(message)) {
+        thread.removeLabel(emailLabel);
+        counter++;
       }
     }
   }
-  return count;
+  return counter;
 }
 
 /**
@@ -31,43 +40,9 @@ function makeDraftMail(): number {
  * @param message - 送信済みメール
  * @return - なし
  */
-function createDraftFromMessage(message: GoogleAppsScript.Gmail.GmailMessage) {
-  // GmailMessageから受信者、件名、本文を取得
-  var recipient = message.getTo(); // 受信者のアドレス
-  var subject = message.getSubject(); // 件名
-  var body = message.getPlainBody(); // 本文（プレーンテキスト）
-
-  // 下書きメールを作成
-  var draft = GmailApp.createDraft(recipient, subject, body);
-  if (draft) {
-    Logger.log("下書きメールが作成されました。");
-  } else {
-    Logger.log("下書きメールの作成に失敗しました。");
-  }
-}
-
-/**
- * メールスレッド（会話のやりとりを１つの塊にまとめたもの）の中から自分の送った最初の送信メールを取得する
- * @param thread - メールスレッド
- * @return 自分の送った最初の送信メール
- */
-function getMessageWithOnlyMeAsSender(thread: GoogleAppsScript.Gmail.GmailThread): GoogleAppsScript.Gmail.GmailMessage | null {
-  const myEmailAddress: string = Session.getActiveUser().getEmail(); // 自分のメールアドレスを取得
-  const messages: GoogleAppsScript.Gmail.GmailMessage[] = thread.getMessages(); // スレッド内のメッセージを取得
-  // for (let i = 0; i < messages.length; i++) {
-  //   Logger.log(i);
-  //   Logger.log(messages[i].getFrom());
-  //   Logger.log(messages[i].getTo());
-  //   Logger.log(messages[i].getSubject());
-  //   Logger.log(messages[i].getPlainBody());
-  //   }
-  const firstMessage: GoogleAppsScript.Gmail.GmailMessage = messages[0]; // スレッド内の最古のメッセージを取得
-  const sender: string = firstMessage.getFrom(); // 発信者を取得
-
-  // 最初のメッセージの発信者が自分のメールアドレスのみを含み、かつ複数の発信者ではない場合
-  if (sender.includes(myEmailAddress) && !sender.includes(",")) {
-    return firstMessage; // メッセージを返す
-  }
-  // 条件を満たすメッセージが見つからない場合
-  return null;
+function createDraftFromMessage(message: GoogleAppsScript.Gmail.GmailMessage): boolean {
+  const recipient = message.getTo(); // 受信者のアドレス
+  const subject = message.getSubject(); // 件名
+  const body = message.getPlainBody(); // 本文（プレーンテキスト）
+  return Boolean(GmailApp.createDraft(recipient, subject, body));
 }
